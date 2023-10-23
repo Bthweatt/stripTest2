71b40b1a5e16c654d4d91afb8bedd9868b384ad7
Bthweatt <67530119+Bthweatt@users.noreply.github.com>
2023-10-23 03:34:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const stripe = require('stripe')('sk_test_51O2yBkGWcIk0Ah5OHbTJF3yhSPZPTVH3OFH7PqikXLo6zIAUBFPUJVtroq6XbQcarj1a9Pu0dskT7iKbbkiTOPne00oGIqGAeG'); // Replace with your actual Stripe secret key
+const cors = require('cors');
 const createPaymentIntent = require('./createPaymentIntent'); // Import the createPaymentIntent function
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Serve static files (HTML, CSS, JS)
 app.use(express.static('public'));
